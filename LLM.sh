99bb12897798980537ea73e1b8a10ca64f1b6b11
Gopi <raju@gmail.com>
2025-10-31 17:55:39 +0530

    added

---
+++ LLM.sh
@@ -0,0 +1,2 @@
+LLM = Large Language Model
+It’s a smart computer program that can read, understand, and write sentences — just like people do!
--- index.py
+++ index.py
@@ -1,1 +1,27 @@
-print("hello Dear");
+import copy #we use python's copys module 
+#create a shallow copy
+
+bag1=[[1,2,3],[4,5,6]]
+#create a shallow copy
+bag2 = copy.copy(bag1)
+print("Before changing anything :")
+print("bag1:", bag1)
+print("bag2:", bag2) 
+bag1[0][0]=999
+print("\nAfter changing anything:")
+print("bag1:",bag1)
+print("bag2:",bag2)
+#======================================
+#deeper copy
+import copy 
+bag1=[[1,2,3],[4,5,6]]
+bag2=copy.deepcopy(bag1)
+print("Before change anything :")
+print("bag1:",bag1)
+print("bag2:",bag2)
+bag1[0][0]=999
+print("after changing anything :")
+print("bag1:",bag1)
+print("bag2:",bag2)
+
+
