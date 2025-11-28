7de52ff387858c561597282d55c1ff2739018f78
Gopi <raju@gmail.com>
2025-11-28 15:28:35 +0530

    added

---
+++ pythonlearn.py
@@ -0,0 +1,42 @@
+#I'm learning Python for ML & AI
+"""
+#print("hello")
+print('im gopi')
+print("i'm Gopi")
+print('o----')
+print(' |||||')
+print('*' *10)
+"""
+
+#==============
+"""
+varibles. 
+#price=10 #int
+price=20 #int
+rating=4.6 #float
+is_published=False #boolean and another we have as complex. 
+print(price)
+"""
+#Excersice 
+'''
+name="john"
+age=20
+is_new=True
+'''
+#Getting input
+'''
+name=input("what is your name? " )
+print("Hi " + name)
+'''
+'''
+name=input("what is you're name? " )
+colour=input("what is you fav colour? " )
+'''
+#====
+#https://agilealliance.org/glossary/tdd/ (Python TDD to practice it)
+
+#====================================================================
+#====================================================================
+
+#Main all the python topics ::
+
