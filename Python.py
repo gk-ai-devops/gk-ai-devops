dbcf2c51d5c9f4febb68fce4461bc5e24fb1c530
Gopi <raju@gmail.com>
2025-12-03 18:41:23 +0530

    updated

--- Python.py
+++ Python.py
@@ -1,1 +1,19 @@
-print("Gopi");
+#varibles
+a=30 #int
+b=12.3 #float
+c="Gopi" #str
+d="True" #bool
+e=[1,2,3] #list
+f={"name":"gopi"} #dict
+print(a,b,c,d,e,f)
+runs=40
+runs=runs+10
+print(runs)
+name="Gopi"
+print(name)
+marks=45
+passed=marks>75
+print(passed)
+marks=94
+fail=marks>76
+print(fail)
